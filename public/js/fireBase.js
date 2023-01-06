739fa11c274000152b05db6d70d12671488f3479
Abed <tiger.connect@hotmail.com>
2023-01-06 22:44:55 +0200

    update .gitignore

---
+++ public/js/fireBase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDv9IlXP2pR7eJwX9jie1L9BtgkLnrQFac",
+  authDomain: "re-remote-control.firebaseapp.com",
+  projectId: "re-remote-control",
+  storageBucket: "re-remote-control.appspot.com",
+  messagingSenderId: "225546835794",
+  appId: "1:225546835794:web:e1d1c6a25359a00792cc8b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
