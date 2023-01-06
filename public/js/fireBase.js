2e092f9826839948bf40c07fbd58761762cf3f1c
Abed <tiger.connect@hotmail.com>
2023-01-06 22:52:38 +0200

    some other fix

--- public/js/fireBase.js
+++ public/js/fireBase.js
@@ -1,8 +1,3 @@
-// Import the functions you need from the SDKs you need
-var { initializeApp } = require("firebase/app");
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDv9IlXP2pR7eJwX9jie1L9BtgkLnrQFac",
@@ -14,4 +9,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
